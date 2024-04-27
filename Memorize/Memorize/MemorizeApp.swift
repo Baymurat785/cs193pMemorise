90b72e86e0ebca75c9d35fdfd2f9280805a3343c
Baymurat785 <123241080+Baymurat785@users.noreply.github.com>
2024-04-27 07:57:07 +0500

    Initial Commit: CardView isFaceUp

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Baymurat Abdumuratov on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
